609bbe5b818ebc294000cab92f9c416311f4b59c
Ayush Raj <148205492+absyhell@users.noreply.github.com>
2025-10-25 18:31:44 +0530

    Refactor sound playback to use makesound function

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
 var x=document.querySelectorAll("button");
-x[0].addEventListener("click",function(){var audio = new Audio("images/sounds/crash.mp3");
-audio.play();});
-x[1].addEventListener("click",function(){var audio = new Audio("images/sounds/kick-bass.mp3");
-audio.play();});
-x[2].addEventListener("click",function(){var audio = new Audio("images/sounds/snare.mp3");
-audio.play();});
-x[3].addEventListener("click",function(){var audio = new Audio("images/sounds/tom-1.mp3");
-audio.play();});
-x[4].addEventListener("click",function(){var audio = new Audio("images/sounds/tom-2.mp3");
-audio.play();});
-x[5].addEventListener("click",function(){var audio = new Audio("images/sounds/tom-3.mp3");
-audio.play();});
-x[6].addEventListener("click",function(){var audio = new Audio("images/sounds/tom-4.mp3");
-audio.play();});
+function makesound(s){
+    var y;
+        switch(s)
+        {
+            case "w":
+                y=new Audio("images/sounds/crash.mp3");
+                y.play();
+                break;
+            case "a":
+                y=new Audio("images/sounds/kick-bass.mp3");
+                y.play();
+                break;
+            case "s":
+                y=new Audio("images/sounds/snare.mp3");
+                y.play();
+                break;
+            case "d":
+                y=new Audio("images/sounds/tom-1.mp3");
+                y.play();
+                break;
+            case "j":
+                y=new Audio("images/sounds/tom-2.mp3");
+                y.play();
+                break;
+            case "k":
+                y=new Audio("images/sounds/tom-3.mp3");
+                y.play();
+                break;
+            case "l":
+                y=new Audio("images/sounds/tom-4.mp3");
+                y.play();
+                break;
+            default: console.log(s);
+        }
+    }
+for(var i=0;i<x.length;i++){
+    x[i].addEventListener("click",function(){
+        var s=this.innerHTML;
+        makesound(s);
+        })
+    }
+
+document.addEventListener("keydown",function(event)
+{
+   s=event.key;
+    makesound(s);
+});
